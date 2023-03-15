31dda81e66ecae1331fb3681efb3809258ae4f20
Christian Ancheta <christian.s.ancheta@gmail.com>
2023-03-15 05:48:18 -0400

    configured cmake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/christianancheta/Documents/GitHub/uncoupled-double-pendulums
+# Build directory: /Users/christianancheta/Documents/GitHub/uncoupled-double-pendulums/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
